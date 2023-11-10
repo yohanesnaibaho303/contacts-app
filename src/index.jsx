6c80b8a08630c5ecf8d08d7398f764ca0484c75e
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-11-10 21:53:52 +0700

    latihan membuat controlled form

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ContactApp from "./components/ContactApp";
+import MyForm from "./components/latihan doang/MyForm";
 
 // styling
 import "./styles/style.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<ContactApp />);
+root.render(<MyForm />);
